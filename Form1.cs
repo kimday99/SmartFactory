4a7750b3262cd3c0b922cbdcba4fbbfc679e6e6b
Daehee Kim <wnqktmvb11@gmail.com>
2024-07-22 22:34:09 +0900

    first com

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+namespace btnproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //버튼1 클릭 시
+
+
+        //버튼2 클릭 시
+
+
+    }
+}
